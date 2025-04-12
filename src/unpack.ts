c3bb8e8e37ffb1a9b69b96013c749a51b1afd2fc
S H <3655508+sh1boot@users.noreply.github.com>
2025-04-11 20:34:04 -0700

    When generating client-side, insert canonical client-side links

--- src/unpack.ts
+++ src/unpack.ts
@@ -6,7 +6,7 @@ export async function unpack() {
     const url = new URL(document.URL);
     const hash = new Uint32Array(await crypto.subtle.digest("SHA-256", enc.encode(url.href)));
     let size = (hash[7] & 0x3fffff) + 0x10000;
-    let gen = pageGenerator(hash, url.search, size, 8192, `${url.pathname}?s=${size>>>10}&q=`);
+    let gen = pageGenerator(hash, url.search, size, 8192, `/cs?s=${size>>>10}&op=${url.pathname}&q=`);
     document.open();
     for (let chunk of gen) {
         document.write(dec.decode(chunk));
